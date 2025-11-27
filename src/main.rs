ece7cacfd12a6d6e155dcfd070c67dbdf1ac9d4c
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-11-27 21:59:24 +0000

    update to use rimage and avif

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use fast_image_resize::{self as fr};
-use image::{ImageReader, imageops};
 use rayon::prelude::*;
-use std::fs;
+use rimage::codecs::avif::{AvifEncoder, AvifOptions};
+use rimage::operations::resize::{Resize, ResizeAlg};
+use std::fs::{self};
+use zune_core::colorspace::ColorSpace;
+use zune_image::traits::{EncoderTrait, OperationsTrait};
+use zune_image::{
+    codecs::{
+        jpeg::JpegDecoder,
+        png::{PngDecoder, zune_core::options::DecoderOptions},
+    },
+    image::Image,
+};
+use zune_imageprocs::composite::CompositeMethod;
 
 use clap::Parser;
 use mimalloc::MiMalloc;
+use zune_core::bytestream::ZCursor;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
@@ -15,35 +26,30 @@ struct Args {
     #[clap(short, long)]
     input: String,
 
-    /// Path to the output directory
-    #[clap(short, long)]
-    output: String,
-
     /// Path to the watermark file
     #[clap(short, long)]
     watermark: Option<String>,
 }
 
 struct Watermark {
-    image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
-    width: u32,
-    height: u32,
+    image: Image,
+    width: usize,
+    height: usize,
 }
 
 fn main() {
     let args = Args::parse();
     const TARGET_WIDTH: f32 = 800.0;
 
-    fs::create_dir_all(&args.output).expect("Failed to create output directory");
+    let decoder_options = DecoderOptions::new_fast();
 
     let watermark_image = args.watermark.as_ref().map(|watermark| {
-        let wm = ImageReader::open(watermark)
-            .unwrap()
-            .decode()
-            .unwrap()
-            .into_rgba8();
-        let wm_w = wm.width();
-        let wm_h = wm.height();
+        let wm_raw = fs::read(watermark).unwrap();
+
+        let decoder = PngDecoder::new_with_options(ZCursor::new(wm_raw), decoder_options);
+
+        let wm = Image::from_decoder(decoder).unwrap();
+        let (wm_w, wm_h) = wm.dimensions();
 
         Watermark {
             image: wm,
@@ -52,93 +58,82 @@ fn main() {
         }
     });
 
-    // iterrate over files in the input directory
     fs::read_dir(args.input)
         .unwrap()
         .par_bridge()
         .for_each(|entry| {
             let entry = entry.expect("Failed to read directory entry");
             let path = entry.path();
+            if path.is_dir() {
+                fs::read_dir(&path)
+                    .unwrap()
+                    .par_bridge()
+                    .for_each(|sub_entry| {
+                        let sub_path = sub_entry
+                            .expect("Failed to read sub-directory entry")
+                            .path();
 
-            // Check if the entry is a file
-            if path.is_file()
-                && let Some(ext) = path.extension()
-                && matches!(ext.to_str().unwrap_or(""), "jpeg" | "jpg")
-            {
-                let img_raw = ImageReader::open(&path).unwrap().decode();
+                        if sub_path.is_file()
+                            && let Some(ext) = sub_path.extension()
+                            && matches!(ext.to_str().unwrap_or(""), "jpeg" | "jpg")
+                        {
+                            let img_raw = fs::read(&sub_path).unwrap();
 
-                if let Err(e) = img_raw {
-                    eprintln!("Failed to decode image {}: {}", path.display(), e);
-                    return;
-                }
-                let img = img_raw.unwrap().into_rgba8();
+                            let decoder = JpegDecoder::new_with_options(
+                                ZCursor::new(img_raw),
+                                decoder_options,
+                            );
+                            let mut img = Image::from_decoder(decoder).unwrap();
+                            img.convert_color(ColorSpace::RGBA).unwrap();
 
-                let (w, h) = (img.width(), img.height());
-                let scale = TARGET_WIDTH / w as f32;
-                let target_height = (h as f32 * scale) as u32;
+                            let (w, h) = img.dimensions();
+                            let scale = TARGET_WIDTH / w as f32;
+                            let target_height = (h as f32 * scale) as usize;
 
-                let mut dst_image = fr::images::Image::new(
-                    TARGET_WIDTH as u32,
-                    target_height,
-                    fast_image_resize::PixelType::U8x4, // img.pixel_type().unwrap(),
-                );
+                            let resize = Resize::new(
+                                TARGET_WIDTH as usize,
+                                target_height,
+                                ResizeAlg::Convolution(
+                                    rimage::operations::resize::FilterType::Bilinear,
+                                ),
+                            );
 
-                let src_img = fr::images::Image::from_vec_u8(
-                    w,
-                    h,
-                    img.into_raw(),
-                    fast_image_resize::PixelType::U8x4,
-                )
-                .unwrap();
+                            resize.execute(&mut img).unwrap();
 
-                let mut resizer = fr::Resizer::new();
+                            if let Some(watermark) = &watermark_image {
+                                let x_offset = (TARGET_WIDTH as usize - watermark.width) / 2;
+                                let y_offset = (target_height - watermark.height) / 2;
+                                let composite = zune_imageprocs::composite::Composite::new(
+                                    &watermark.image,
+                                    CompositeMethod::Over,
+                                    (x_offset, y_offset),
+                                );
+                                composite.execute(&mut img).unwrap();
+                            }
 
-                resizer
-                    .resize(
-                        &src_img,
-                        &mut dst_image,
-                        &fr::ResizeOptions {
-                            algorithm: fr::ResizeAlg::Convolution(fr::FilterType::Box),
-                            cropping: fr::SrcCropping::None,
-                            mul_div_alpha: false,
-                        },
-                    )
-                    .unwrap();
+                            let avif_encoder_options = AvifOptions {
+                                speed: 1,
+                                ..Default::default()
+                            };
 
-                let dst_w = dst_image.width();
-                let dst_h = dst_image.height();
+                            let mut avif_encoder =
+                                AvifEncoder::new_with_options(avif_encoder_options);
 
-                let mut out_img = image::RgbaImage::from_raw(
-                    TARGET_WIDTH as u32,
-                    target_height,
-                    dst_image.buffer().to_vec(),
-                )
-                .unwrap();
+                            let mut result = vec![]; // TODO: pre-allocate with capacity
 
-                if let Some(watermark) = &watermark_image {
-                    imageops::overlay(
-                        &mut out_img,
-                        &watermark.image,
-                        ((dst_w - watermark.width) / 2) as i64,
-                        ((dst_h - watermark.height) / 2) as i64,
-                    );
-                }
+                            avif_encoder.encode(&img, &mut result).unwrap();
 
-                // OPTIMIZE: Use the `libwebp-sys` crate to convert to webp
-                let out_img = image::DynamicImage::from(out_img);
-
-                let encoder = webp::Encoder::from_image(&out_img).unwrap();
-
-                let webp_data = encoder.encode(40.0);
-
-                let filename_without_extension =
-                    path.file_stem().unwrap().to_string_lossy().to_string();
-
-                std::fs::write(
-                    format!("{}/{}.webp", args.output, filename_without_extension),
-                    &*webp_data,
-                )
-                .unwrap();
+                            let processed_dir = path.join("processed");
+                            fs::create_dir_all(&processed_dir).unwrap();
+                            let filename_without_extension =
+                                sub_path.file_stem().unwrap().to_string_lossy().to_string();
+                            fs::write(
+                                processed_dir.join(format!("{}.avif", filename_without_extension)),
+                                &result,
+                            )
+                            .unwrap();
+                        }
+                    });
             }
         });
 
